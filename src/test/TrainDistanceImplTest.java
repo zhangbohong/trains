4de5ad2b26ea25ef645edd60ff922b59078f563d
zhangbohong <zhangbohong@kyee.com.cn>
2018-05-06 20:22:49 +0800

    完成根据始末点和最短stop输出行程 完成根据始末点和最短距离输出行程

---
+++ src/test/TrainDistanceImplTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.Assert.*;
+
+public class TrainDistanceImplTest {
+
+    @org.junit.Test
+    public void getMinStep() {
+    }
+
+    @org.junit.Test
+    public void getRouteByStops() {
+    }
+
+    @org.junit.Test
+    public void getRouteByDistance() {
+    }
+
+    @org.junit.Test
+    public void getDistanceByRoute() {
+    }
+
+    @org.junit.Test
+    public void initStepLength() {
+    }
+}
---
+++ src/test/trainsTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class trainsTest {
+
+    @Test
+    public void main() {
+    }
+}
